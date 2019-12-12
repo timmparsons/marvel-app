3027d28daaaf226e52508386575763619765540c
Tim Parsons <timmparsons85@gmail.com>
2019-12-12 13:05:39 -0700

    Updates

---
+++ precache-manifest.3e20acda060c946f496752bd5e75f8c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3671900d66d1a98715f0ece192a8f2bd",
+    "url": "/marvel-app/index.html"
+  },
+  {
+    "revision": "323e9460ab294b9de52e",
+    "url": "/marvel-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9568a80aaa15c9c9793b",
+    "url": "/marvel-app/static/js/2.c8204845.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/marvel-app/static/js/2.c8204845.chunk.js.LICENSE"
+  },
+  {
+    "revision": "323e9460ab294b9de52e",
+    "url": "/marvel-app/static/js/main.a7666b80.chunk.js"
+  },
+  {
+    "revision": "5644d38471e8f5647d6b",
+    "url": "/marvel-app/static/js/runtime-main.5044610b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/marvel-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
